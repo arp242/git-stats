980fb234e5091d45a43607c1b3c7bb9be92cfb0c
Martin Tournoij <martin@arp242.net>
2024-04-10 11:09:30 +0100

    Slightly nicer table sorting

--- tpl/tsort.js
+++ tpl/tsort.js
@@ -1,10 +1,34 @@
+// tsort sorts tables.
+//
+// Use by calling tsort(sel, strcmp), where sel is a list of headers to bind to.
+// The default when omitted is 'table.tsort >thead th'
+//
+// strcmp can be used to pass a string comparison function; the default is
+// Intl.Collator().compare, but you can give your own function for a different
+// locale, pass any Collator() options, or implement something custom entirely.
+//
+// Headers with the class "n" will be sorted numerically.
+//
+// Only rows in the first tbody will be sorted.
+//
+// data-tsort will be set to "asc" or "desc" for th that's currently sorted.
+//
+// The initial sort state can be set by adding that to a header. If it's not
+// given, tsort-asc will be added to the first header.
+//
+// https://github.com/arp242/tsort.js | MIT license applies, see LICENSE.
 ;(function() {
 	'use strict';
 
-	window.tsort = function(sel) {
+	window.tsort = function(sel, strcmp) {
 		if (sel === undefined)
-			sel = 'table.tsort th'
+			sel = 'table.tsort >thead th'
+		if (strcmp === undefined)
+			strcmp = Intl.Collator().compare
+
+		let initial = false
 		document.querySelectorAll(sel).forEach(function(th) {
+			initial = initial || th.classList.contains('tsort-asc') || th.classList.contains('tsort-desc')
 			th.addEventListener('click', function(e) {
 				let num_sort = th.classList.contains('n'),
 					col      = Array.from(th.parentNode.children).indexOf(th),
@@ -12,12 +36,12 @@
 					tbody    = tbl.querySelector('tbody'),
 					rows     = Array.from(tbody.children).filter((e) => e.tagName === 'TR'),
 					to_i     = (i) => parseFloat(i.replace(/,/g, '')),
-					is_sort  = th.dataset.sort === '1'
+					is_sort  = th.classList.contains('tsort-asc')
 
 				if (num_sort)
 					rows.sort((a, b) => to_i(b.children[col].innerText) - to_i(a.children[col].innerText))
 				else
-					rows.sort((a, b) => a.children[col].innerText.localeCompare(b.children[col].innerText))
+					rows.sort((a, b) => strcmp(a.children[col].innerText, b.children[col].innerText))
 
 				if (is_sort)
 					rows.reverse()
@@ -25,9 +49,11 @@
 				tbody.innerHTML = ''
 				rows.forEach((r) => tbody.appendChild(r))
 
-				tbl.querySelectorAll('th').forEach((t) => t.dataset.sort = '0')
-				th.dataset.sort = is_sort ? '0' : '1'
-			}, false)
+				tbl.querySelectorAll('th').forEach((t) => t.classList.remove('tsort-asc', 'tsort-desc'))
+				th.classList.add(is_sort ? 'tsort-desc' : 'tsort-asc')
+			})
 		})
+		if (!initial)
+			document.querySelectorAll('.tsort >thead th')[0].classList.add('tsort-asc')
 	}
 })();
--- update-all.zsh
+++ update-all.zsh
@@ -4,8 +4,8 @@ set -eu
 repos=($(psql -XtA git-stats -c "select name from repos"))
 integer i
 for r in $repos; do
-	i+=i
-	print -f '\r\x1b[2K\x1b[1m[%d / %d] %s\x1b[0m' $r $i $#repos
-	git-stats update $r
+        i+=1
+        print -f '\x1b[1m[%d / %d] %s\x1b[0m\n' $i $#repos $r
+        ./git-stats -db 'psql+dbname=git-stats sslmode=disable' update $r ||:
 done
 print
